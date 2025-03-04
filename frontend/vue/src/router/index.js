ed5d77775e52dad78fb02e0c244918f4b847b82d
xle6yc3k <huntergeorgod@gmail.com>
2025-03-04 21:07:40 +0300

    Добавлены заглушки на все основные страницы, чтобы проработать маршрутизацию

--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-// import HomePage from '../pages/HomePage.vue';
-// import ResumeBuilder from '../pages/ResumeBuilderPage.vue';
-// import AnalysePage from '../pages/AnalysePage.vue';
-// import CombCheckPage from '../pages/CombCheckPage.vue';
-// import ProfilePage from '../pages/ProfilePage.vue';
+import HomePage from '../pages/HomePage.vue';
+import ResumeBuilder from '../pages/ResumeBuilderPage.vue';
+import AnalysePage from '../pages/AnalysePage.vue';
+import CombCheckPage from '../pages/CombCheckPage.vue';
+import ProfilePage from '../pages/ProfilePage.vue';
 
 const routes = [
-//   { path: '/', component: HomePage },
-//   { path: '/resume-builder', component: ResumeBuilder },
-//   { path: '/analyse', component: AnalysePage },
-//   { path: '/comb-check', component: CombCheckPage},
-//   { path: '/profile', component: ProfilePage },
+  { path: '/', component: HomePage },
+  { path: '/resume-builder', component: ResumeBuilder },
+  { path: '/analyse', component: AnalysePage },
+  { path: '/comb-check', component: CombCheckPage},
+  { path: '/profile', component: ProfilePage },
 ];
 
 const router = createRouter({
