59e31ca59e7bad35911796ac4d3c343e5d564215
xle6yc3k <huntergeorgod@gmail.com>
2025-03-04 21:16:38 +0300

    исправлен баг с роутером, установлены зависимости для бута и иконок

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js';
+import './style.css';
 
-createApp(App).mount('#app')
+// Импорт иконок
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// Импорт Bootstrap 5
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
