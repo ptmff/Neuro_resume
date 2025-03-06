1e4a386f0761ba5aa766ccf11de3f988fd86ba27
fuglyy <sp.yanusya@gmail.com>
2025-03-06 15:11:28 +0300

    +ResumePreview

--- frontend/vue/src/pages/ResumeBuilderPage.vue
+++ frontend/vue/src/pages/ResumeBuilderPage.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="page">
     <ProgressBar/>
-    <ResumeForm/>
+    <ResumeForm :resumeData="resumeData"/>
+    <ResumePreview :resumeData="resumeData"/>
   </div>
 </template>
 
 <script>
 import ResumeForm from '../components/ResumeForm.vue';
 import ProgressBar from '../components/ProgressBar.vue';
+import ResumePreview from '../components/ResumePreview.vue';
 
 export default{
 components:{
   ResumeForm,
-  ProgressBar
+  ProgressBar,
+  ResumePreview
 },
+ data() {
+  return{
+    resumeData: {
+        name: '',
+        email: '',
+        phone: '',
+        location: '',
+        profession: '',
+        education: '',
+        experience: '',
+        skills: ''
+      }
+  }
+ }
+
 };
 </script>
 
