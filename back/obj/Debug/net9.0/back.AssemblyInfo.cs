3dd6310e16a3f42eacb08d94bd08726f7c41eb36
Denis <ddm22026@gmail.com>
2025-03-06 14:52:28 +0300

    add obj

--- back/obj/Debug/net9.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da29f441ab0957247b2eaabcf8814d6694ef1d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d26c39da6ce09f5a36b2d38e412721e88e62551")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
