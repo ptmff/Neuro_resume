3c9e3426b9526ef68dfcf4197c98735e0791c990
n1ck0n <novozeninnikita2@gmail.com>
2025-03-07 16:45:40 +0300

    создан компонент преимущества

---
+++ frontend/vue/src/components/AdvantagesSection.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="advantages fade-up">
+      <div class="container">
+        <h2 class="section-title text-center">Почему выбирают NeuroResume?</h2>
+        
+        <div class="advantages-content">
+          <!-- Список преимуществ -->
+          
+        </div>
+      </div>
+    </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import aiGen1 from '../assets/ai-gen1.jpg'
+
+const activeIndex = ref(0);
+
+const setActiveAdvantage = (index) => {
+  activeIndex.value = index;
+};
+</script>
