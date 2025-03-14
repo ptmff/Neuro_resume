be14618f9092d1be4b51025126f983ab398ad5a3
Denis <ddm22026@gmail.com>
2025-03-14 16:02:39 +0300

    add models

--- back/back/Models/Resume.cs
+++ back/back/Models/Resume.cs
@@ -1,9 +1,14 @@
 namespace back.Models;
 
-public class Resume{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime Date { get; set; }
-    public string job { get; set; }
-    public List<string> skills { get; set; }
-}
+public class Resume
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } // Внешний ключ
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Job { get; set; } // Изменил на Job с заглавной буквы для соответствия стилю
+        public List<string> Skills { get; set; } // Изменил на Skills с заглавной буквы для соответствия стилю
+
+        // Навигационное свойство
+        public User User { get; set; } // Связь с пользователем
+    }
