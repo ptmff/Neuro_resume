1e760156c256fec788350ad2b4f34ccdde6a235b
Denis <ddm22026@gmail.com>
2025-03-14 15:35:26 +0300

    fix models

--- back/back/Data/AppDbContext.cs
+++ back/back/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using back.Models;
 
 namespace back.Data;
 
--- back/back/obj/Debug/net9.0/back.AssemblyInfo.cs
+++ back/back/obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38779688c428fc2534799f5c4572f78d3cf0298b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7778b0129d277c8eb968aadbba6504b0b5f77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
