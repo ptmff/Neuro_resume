b9d4befec1687950b4d88dad52340a292af29fc1
Denis <ddm22026@gmail.com>
2025-03-14 17:40:04 +0300

    fix migrations

--- back/back/Data/AppDbContext.cs
+++ back/back/Data/AppDbContext.cs
@@ -2,28 +2,27 @@
 using System.Collections.Generic;
 using back.Models;
 
-namespace back.Data;
-
-public class AppDbContext : DbContext
+namespace back.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<User> Users { get; set; }
-    public DbSet<Resume> Resumes { get; set; }
-
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    // Настройка связи один-ко-многим
-    //    modelBuilder.Entity<User>()
-    //        .HasMany(u => u.Resumes)
-    //        .WithOne(r => r.User)
-    //        .HasForeignKey(r => r.UserId);
-    //}
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class AppDbContext : DbContext
     {
-        modelBuilder.Entity<Resume>()
-            .Property(r => r.Skills)
-            .HasColumnType("jsonb");
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка связи один-ко-многим
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Resumes)
+                .WithOne(r => r.User)
+                .HasForeignKey(r => r.UserId);
+
+            // Настройка типа данных для поля Skills
+            modelBuilder.Entity<Resume>()
+                .Property(r => r.Skills)
+                .HasColumnType("jsonb");
+        }
     }
 }
--- back/back/Migrations/20250314143841_Migrations.Designer.cs
+++ back/back/Migrations/20250314143841_Migrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using back.Data;
@@ -12,9 +13,11 @@ using back.Data;
 namespace back.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250314143841_Migrations")]
+    partial class Migrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
