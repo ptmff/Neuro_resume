858413a7587d404b49e715e093b6624f80d17e8b
Denis <ddm22026@gmail.com>
2025-03-14 15:46:44 +0300

    add new table

--- back/back/Data/AppDbContext.cs
+++ back/back/Data/AppDbContext.cs
@@ -9,4 +9,5 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Resume> Resumes { get; set; }
 }
