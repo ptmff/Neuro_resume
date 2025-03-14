80776341ad60f7a3455da7be514b112c76498598
Denis <ddm22026@gmail.com>
2025-03-14 16:03:32 +0300

    fix database

--- back/back/Data/AppDbContext.cs
+++ back/back/Data/AppDbContext.cs
@@ -10,4 +10,13 @@ public class AppDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<Resume> Resumes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Настройка связи один-ко-многим
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Resumes)
+            .WithOne(r => r.User)
+            .HasForeignKey(r => r.UserId);
+    }
 }
